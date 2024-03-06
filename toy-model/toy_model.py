b6e2945cc8295b88bd82206b126ab0f489821a3f
Shiqi-Wu <18962528568@163.com>
2024-03-06 16:21:54 +0800

    Initial commit

---
+++ toy-model/toy_model.py
@@ -0,0 +1,54 @@
+import numpy as np
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+from sklearn.model_selection import train_test_split
+
+class diffusion_equation(object):
+    def __init__(self, dim = 20, L0 = 0, L1 = 1, mu = [1, 1]):
+        self.dim = dim
+        self.L0 = L0
+        self.L1 = L1
+        self.mu = mu
+        self.build_A()
+
+    def build_A(self):
+        self.A = np.zeros([self.dim - 1, self.dim - 1])
+        for i in range(self.dim - 1):
+            self.A[i, i] = -2 
+        for i in range(self.dim - 2):
+            self.A[i, i + 1] = 1
+            self.A[i + 1, i] = 1
+    
+    def generate_traj(self, steps, u0, dlt_t = 0.001):
+        u_data = [u0]
+        for _ in range(steps):
+            u0 = u0 + dlt_t * (self.mu[0] * self.A @ u0 + self.mu[1] * u0 @ self.A)
+            u_data.append(u0)
+        return np.array(u_data)
+    
+    def generate_training_data(self, traj_num, steps, dlt_t = 0.001):
+        u0 = np.random.rand(self.dim - 1, self.dim - 1)
+        u_data = self.generate_traj(steps, u0, dlt_t)
+        u_x = u_data[:-1,:,:]
+        u_y = u_data[1:, :, :]
+        for i in range(traj_num - 1):
+            u0 = np.random.rand(self.dim - 1, self.dim - 1)
+            u = self.generate_traj(steps, u0, dlt_t)
+            u_data = np.concatenate((u_data, u), axis = 0)
+            u_x = np.concatenate((u_x, u[:-1,:,:]), axis = 0)
+            u_y = np.concatenate((u_y, u[1:,:,:]), axis = 0)
+        u_x_lace_1 = u_x @ self.A
+        u_x_lace_2 = self.A @ u_x
+        data_x = np.reshape(u_x, (-1, 1))
+        data_y = np.reshape(u_y, (-1, 1))
+        data_lace_1 = np.reshape(u_x_lace_1, (-1,1))
+        data_lace_2 = np.reshape(u_x_lace_2, (-1,1))
+
+        u_x_tensor = torch.tensor(u_x, dtype=torch.float32)
+        u_y_tensor = torch.tensor(u_y, dtype=torch.float32)
+        u_x_lace_1_tensor = torch.tensor(u_x_lace_1, dtype=torch.float32)
+        u_x_lace_2_tensor = torch.tensor(u_x_lace_2, dtype=torch.float32)
+
+        dataset = TensorDataset(u_x_tensor, u_y_tensor, u_x_lace_1_tensor, u_x_lace_2_tensor)
+    
+        return dataset
